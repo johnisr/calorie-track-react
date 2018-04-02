72d0be8b6180fb69f3e7b3c9e76586ca4e335e90
John <john.i.s.ramos@gmail.com>
2018-04-02 16:04:01 -0400

    Add react-dates for creating logs with other dates

--- src/components/LogDashboardPage.js
+++ src/components/LogDashboardPage.js
@@ -1,10 +1,70 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
 import LogList from './LogList';
+import { addCurrentEditLog } from '../actions/currentLog';
+import { defaultLog, addLog } from '../actions/logs';
 
-const LogDashboardPage = (props) => (
-  <div>
-    <LogList history={props.history}/>
-  </div>
-);
+export class LogDashboardPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calendarFocused: false,
+      date: moment(),
+    };
+  }
+  onDateChange = (date) => {
+    if (date) {
+      this.setState(() => ({ date }));
+    }
+  }
+  onFocusChange = ({ focused }) => { 
+    this.setState(() => ({ calendarFocused: focused }));
+  }
+  handleCreate = () => {
+    const date = this.state.date.startOf('day').valueOf();
+    const log = this.props.logs.filter(log => log.date === date)[0];
+    if (!log) {
+      const newLog = {
+        ...defaultLog,
+        date,
+      };
+      this.props.addLog(newLog);
+      this.props.addCurrentEditLog(newLog);
+    } else {
+      this.props.addCurrentEditLog(log);
+    }
+    this.props.history.push('/editlog');
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <SingleDatePicker
+            date={this.state.date}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+          />
+          <button onClick={this.handleCreate}>Create Log</button>
+        </div>
+        <LogList history={this.props.history}/>
+      </div>
+    );
+  }
+}
 
-export default LogDashboardPage;
+const mapStateToProps = (state) => ({
+  logs: state.logs,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addLog: (log) => dispatch(addLog(log)),
+  addCurrentEditLog: (log) => dispatch(addCurrentEditLog(log)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogDashboardPage);
