a816c4ab760965de1fe820269aed19b911e54218
John <john.i.s.ramos@gmail.com>
2018-04-09 11:51:21 -0400

    Add loading animation, refactor index.js so that it only renders App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
 import React from 'react';
 
-import AppRouter from './routers/AppRouter';
-
 import './App.css';
 
-const App = () => (
-  <AppRouter />
-);
+import AppRouter from './routers/AppRouter';
+import { Provider } from 'react-redux';
+import { history } from './routers/AppRouter';
+
+import configureStore from './store/configureStore';
+import { firebase } from './firebase/firebase';
+
+import { login, logout } from './actions/auth';
+import { startSetFoods } from './actions/foods';
+import { startSetLogs } from './actions/logs';
+
+// import Playground from './playground/log';
+// import EditFood from './playground/editFood';
+// import CurrentLog from './playground/currentLog';
+// import Tabs from './playground/tabs';
+// import Tabs from './playground/compoundComponentTabs';
+// import PlayForm from './playground/PlayForm';
+// import logs from './tests/fixtures/logs';
+// import { addLog } from './actions/logs';
+
+const store = configureStore();
+
+class App extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        store.dispatch(login(user.uid));
+        try {
+          await Promise.all([
+            store.dispatch(startSetFoods()),
+            store.dispatch(startSetLogs()),
+          ]);
+          const path = history.location.pathname;
+          if (path === '/') {
+            history.push('/FoodDashboard');
+          }
+          this.setState({ loading: false });
+        } catch(e) {
+    
+        }
+      } else {
+        store.dispatch(logout());
+        history.push('/');
+        this.setState({ loading: false });
+      }
+    });
+  };
+
+  render() {
+    if(this.state.loading) {
+      return null;
+    }
+
+    return (
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    );
+  };
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,66 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { Provider } from 'react-redux';
-import { history } from './routers/AppRouter';
-
-import configureStore from './store/configureStore';
-import { firebase } from './firebase/firebase';
-
-import { login, logout } from './actions/auth';
-import { startSetFoods } from './actions/foods';
-import { startSetLogs } from './actions/logs';
-
-// import Playground from './playground/log';
-// import EditFood from './playground/editFood';
-// import CurrentLog from './playground/currentLog';
-// import Tabs from './playground/tabs';
-// import Tabs from './playground/compoundComponentTabs';
-// import PlayForm from './playground/PlayForm';
-// import logs from './tests/fixtures/logs';
-// import { addLog } from './actions/logs';
-
-const store = configureStore();
-
-const jsx = (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-
-let hasRendered = false;
-const renderApp = () => {
-  if (!hasRendered) {
-    ReactDOM.render(jsx, document.getElementById('root'));
-    hasRendered = true;
-  }
-}
-
-ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
-
-firebase.auth().onAuthStateChanged(async (user) => {
-  if (user) {
-    store.dispatch(login(user.uid));
-    try {
-      await Promise.all([
-        store.dispatch(startSetFoods()),
-        store.dispatch(startSetLogs()),
-      ]);
-      renderApp();
-      const path = history.location.pathname;
-      if (path === '/') {
-        history.push('/FoodDashboard');
-      }
-    } catch(e) {
-
-    }
-  } else {
-    store.dispatch(logout());
-    renderApp();
-    history.push('/');
-  }
-});
+ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
